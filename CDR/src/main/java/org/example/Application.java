d9cea645a8a59ed41e51baa35df4b1fc6a7363ef
DyukovNA <1ceptnpd@gmail.com>
2025-04-11 02:28:48 +0300

    R-2 CDR hotfixes

--- CDR/src/main/java/org/example/Application.java
+++ CDR/src/main/java/org/example/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Основной класс приложения, который запускает Spring Boot приложение.
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication
 @ComponentScan
+@PropertySource("classpath:application.properties")
 public class Application {
     /**
      * Точка входа в приложение.
