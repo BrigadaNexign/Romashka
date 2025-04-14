48bae754ca48eaf080c80747c43eed81e0cab589
Sergey <165892155+DSergeyA@users.noreply.github.com>
2025-04-14 19:33:22 +0300

    Create tables for BRT
    Скрипт для создания таблиц в БД BRT в соответствии с ER диаграммами.

---
+++ resources/db/changelog/v.0.0.2/2025-04-14-3-create-tables-brt.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "users_saved" (
+	"user_id"	INTEGER NOT NULL UNIQUE,
+	"user_name"	TEXT NOT NULL,
+	"tariff_id"	INTEGER NOT NULL,
+	"msisdn"	CHAR(11) NOT NULL UNIQUE,
+	"balance"	NUMERIC(10, 2) NOT NULL,
+	"registration_date"	DATE NOT NULL,
+	PRIMARY KEY("user_id" AUTOINCREMENT),
+	FOREIGN KEY("tariff_id") REFERENCES "tariffs"("tariff_id") ON DELETE SET NULL
+);
+
+CREATE TABLE "user_params" (
+	"user_id"	INTEGER NOT NULL,
+	"payment_day"	DATE,
+	"minutes"	INTEGER,
+	FOREIGN KEY("user_id") REFERENCES "users_saved"("user_id") ON DELETE CASCADE
+);
