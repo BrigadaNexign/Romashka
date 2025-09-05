79a4bb4e9566b423e5d02984cbeb2094450b1101
DyukovNA <1ceptnpd@gmail.com>
2025-09-05 22:51:59 +0300

    R-9 Kafka

--- BRT/src/main/java/rom/brt/BrtApplication.java
+++ BRT/src/main/java/rom/brt/BrtApplication.java
@@ -1,6 +1,5 @@
 package rom.brt;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.PropertySource;
  */
 @SpringBootApplication
 @EnableFeignClients
-@EnableRabbit
 @PropertySource("classpath:application.properties")
 public class BrtApplication {
     public static void main(String[] args) {
